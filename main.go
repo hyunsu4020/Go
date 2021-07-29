174b15ff6240bdd6232389d2505d6ed894234c0e
hyunsu4020 <76948939+hyunsu4020@users.noreply.github.com>
2021-07-29 14:15:17 +0900

    Array and slices.go

--- main.go
+++ main.go
@@ -2,14 +2,8 @@ package main
 
 import "fmt"
 
-type person struct {
-	name    string
-	age     int
-	favFood []string
-}
-
 func main() {
-	favFood := []string{"kimchi", "ramen"}
-	nico := person{name: "nico", age: 18, favFood: favFood}
-	fmt.Println(nico)
+	names := []string{"hyunsu", "lynn", "dal"}
+	names = append(names, "flynn")
+	fmt.Println(names)
 }

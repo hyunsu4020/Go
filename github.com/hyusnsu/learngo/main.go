67e2d178b118e174359058da344c964378d39682
hyunsu4020 <76948939+hyunsu4020@users.noreply.github.com>
2021-08-02 18:49:17 +0900

    Add files via upload

--- github.com/hyusnsu/learngo/main.go
+++ github.com/hyusnsu/learngo/main.go
@@ -2,16 +2,18 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/hyusnsu/learngo/mydict"
+	"time"
 )
 
 func main() {
-	dictionary := mydict.Dictionary{"first": "First word"}
-	definition, err := dictionary.Search("first")
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(definition)
+	go sexyCount("nico")
+	go sexyCount("flynn")
+	time.Sleep(time.Second * 5)
+}
+
+func sexyCount(person string) {
+	for i := 0; i < 10; i++ {
+		fmt.Println(person, "is sexy", i)
+		time.Sleep(time.Second)
 	}
 }
--- github.com/hyusnsu/learngo/mydict/mydict.go
+++ github.com/hyusnsu/learngo/mydict/mydict.go
@@ -1,11 +1,17 @@
 package mydict
 
-import "errors"
+import (
+	"errors"
+)
 
 // Dictionary type
 type Dictionary map[string]string
 
-var errNotFound = errors.New("Not Found")
+var (
+	errNotFound   = errors.New("Not Found")
+	errCanUpdate  = errors.New("Cant update non-existing word")
+	errWordExists = errors.New("That word already exists")
+)
 
 // Search for a word
 func (d Dictionary) Search(word string) (string, error) {
@@ -14,4 +20,33 @@ func (d Dictionary) Search(word string) (string, error) {
 		return value, nil
 	}
 	return "", errNotFound
+}
+
+// Add a word to the dictionary
+func (d Dictionary) Add(word, def string) error {
+	_, err := d.Search(word)
+	switch err {
+	case errNotFound:
+		d[word] = def
+	case nil:
+		return errWordExists
+	}
+	return nil
+}
+
+// Update a word
+func (d Dictionary) Update(word, definition string) error {
+	_, err := d.Search(word)
+	switch err {
+	case nil:
+		d[word] = definition
+	case errNotFound:
+		return errCanUpdate
+	}
+	return nil
+}
+
+// Delete a word
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
 }

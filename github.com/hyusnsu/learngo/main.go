8b9f61a9a5e7f668c029f97f67fbbbfcdc69fa99
hyunsu4020 <76948939+hyunsu4020@users.noreply.github.com>
2021-08-05 20:37:36 +0900

    Add files via upload

--- github.com/hyusnsu/learngo/main.go
+++ github.com/hyusnsu/learngo/main.go
@@ -1,46 +1,51 @@
 package main
 
-import (
+import {
 	"fmt"
 	"net/http"
-)
-
-type requestResult struct {
-	url    string
-	status string
+	"github.com/PuerkitoBio/goquery"
 }
+var baseURL string = ""https://kr.indeed.com/jobs?q=python&limit=50""
 
-func main() {
-	results := make(map[string]string)
-	c := make(chan requestResult)
-	urls := []string{
-		"https://www.airbnb.com/",
-		"https://www.google.com/",
-		"https://www.amazon.com/",
-		"https://www.reddit.com/",
-		"https://www.google.com/",
-		"https://soundcloud.com/",
-		"https://www.facebook.com/",
-		"https://www.instagram.com/",
-		"https://academy.nomadcoders.co/",
-	}
-	for _, url := range urls {
-		go hitURL(url, c)
-	}
-	for i := 0; i < len(urls); i++ {
-		result := <-c
-		results[result.url] = result.status
-	}
-	for url, status := range results {
-		fmt.Println(url, status)
+func main {
+	totalPages := getPages()
+
+	for i := 0; i < totalPages; i++ {
+		getPage(i)
 	}
 }
 
-func hitURL(url string, c chan<- requestResult) {
-	resp, err := http.Get(url)
-	status := "OK"
-	if err != nil || resp.StatusCode >= 400 {
-		status = "FAILED"
+func getPages(page int) {
+	pageURL := baseURL + "&start=" + strconv.Itoa(page*50)
+	fmt.Println("Requesting", pageURL)
+}
+
+func getPages int {
+	pages := 0
+	res, err := http.Get(baseURL)
+	checkErr(err)
+	checkCode(res)
+	
+	defer res.Boby.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	doc.Find(".pagination").Each(func (i int, *goquery.Selection)  {
+		pages = s.Find("a").Length())
+	})
+	
+	return pages
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatalln(err)
 	}
-	c <- requestResult{url: url, status: status}
+}
+
+func checkCode(res *http.Response) {
+	if res.StatusCode != 200 {
+		log.Fatalln("Request failed with Status:", res.StatusCode)	
+	}
 }
